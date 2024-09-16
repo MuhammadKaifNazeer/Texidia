6ed83a6ae0325d351af15a2e9c122c3d51513e9b
Muhammad Kaif Nazeer <34789210-muhammadkaif057@users.noreply.replit.com>
2024-09-16 07:47:01 +0000

    added view code button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import { ThemeToggler } from "@/components/ThemeToggler/ThemeToggler";
-import { Button } from "@/components/ui/button";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
-import Link from "next/link";
 import App from "@/components/App/App";
 
 export default function Home() {
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { ThemeToggler } from "../ThemeToggler/ThemeToggler";
 import { ShareButton } from "../ShareButton/ShareButton";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
@@ -10,7 +11,12 @@ const Navbar: React.FC = () => {
         <div className="flex items-center justify-between w-full md:h-16 px-2 sm:px-4 md:px-6 py-4 max-w-7xl mx-auto">
           <h2 className="text-lg font-semibold">Texidia</h2>
           <div className="flex space-x-2 justify-end">
-            <Button variant={"outline"}>View Code</Button>
+            <Link
+              href={"https://github.com/MuhammadKaifNazeer/Texidia.git"}
+              target="_blank"
+            >
+              <Button variant={"outline"}>View Code</Button>
+            </Link>
             <ShareButton />
             <ThemeToggler />
           </div>
